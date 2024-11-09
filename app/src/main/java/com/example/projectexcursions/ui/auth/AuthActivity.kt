589d103d0c0db22bab8bc322c81b7b34b518720e
dvrk_rave <ivorkov105@gmail.com>
2024-11-10 01:02:41 +0700

    Сверстал активити для авторизации и регистрации, проинициализировал их, также присутствует тестовая логика для кнопки входа, была попытка реализовать кнопку перехода к окну регистрации, но её функционал ограничевается крашем приложения

---
+++ app/src/main/java/com/example/projectexcursions/ui/auth/AuthActivity.kt
@@ -0,0 +1,41 @@
+package com.example.projectexcursions.ui.auth
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projectexcursions.databinding.ActivityAuthBinding
+import com.example.projectexcursions.ui.registration.RegActivity
+
+class AuthActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+    private val viewModel: AuthViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initCallback()
+        subscribe()
+    }
+
+    private fun subscribe() {
+        viewModel.loginStatus.observe(this) { value ->
+            Toast.makeText(this, value.toString(), Toast.LENGTH_SHORT).show()
+        }
+        viewModel.wantReg.observe(this) { value ->
+            if (value) {
+                startActivity(Intent(this@AuthActivity, RegActivity::class.java))
+                viewModel.goneToReg()
+            }
+        }
+    }
+
+
+
+    private fun initCallback() {
+        binding.buttAuth.setOnClickListener { viewModel.clickAuth() }
+        binding.buttReg.setOnClickListener { viewModel.clickRegister() }
+    }
+}
---
+++ app/src/main/java/com/example/projectexcursions/ui/registration/RegViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.projectexcursions.ui.registration
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class RegViewModel: ViewModel() {
+    //TODO обзяательная проверка логина и пароля, если такие существуют - выдать исключение с сообщшением или просто сообщение с ошибкой
+
+    private val _regStatus = MutableLiveData<Boolean>()
+    val regStatus: LiveData<Boolean> get() = _regStatus
+
+    fun reg(login: String, password: String) {
+        //todo скинуть логин и пароль бекенду
+    }
+
+    fun clickRegister() {
+        TODO("Not yet implemented")
+    }
+}
