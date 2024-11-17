f9a8f7fc210d017ffb6fd2b2eccd701b7cc89848
dvrk_rave <ivorkov105@gmail.com>
2024-11-18 03:51:28 +0700

    делаю приём данных с бекенда

--- app/src/main/java/com/example/projectexcursions/models/Excursion.kt
+++ app/src/main/java/com/example/projectexcursions/models/Excursion.kt
@@ -1,5 +1,8 @@
 package com.example.projectexcursions.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Excursion(
     val title: String,
     val description: String
