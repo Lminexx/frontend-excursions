6e5d8b5ecec4e8180b872f1091729e64f1c06bd8
dvrk_rave <38814129+ivorkov105@users.noreply.github.com>
2024-11-22 17:38:30 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,7 @@ dependencies {
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx) 
     implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.room.ktx)
 
     kapt(libs.room.compiler)
 
