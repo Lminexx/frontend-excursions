d4ee6de7dc9e36661223c8c158c6b63a3345d1f0
Ximyl <azikyn234@mail.ru>
2024-11-27 11:37:20 +0700

    Добавил проверку токена и auth

--- app/src/main/java/com/example/projectexcursions/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/projectexcursions/ui/fragments/ProfileFragment.kt
@@ -1,39 +1,47 @@
 import androidx.fragment.app.Fragment
 import com.example.projectexcursions.R
-
-
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.projectexcursions.MyApplication.Companion.getAuthToken
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.projectexcursions.token_bd.TokenRepository
 import com.example.projectexcursions.ui.auth.AuthActivity
-import com.example.projectexcursions.ui.registration.RegActivity
+import com.example.projectexcursions.ui.auth.AuthViewModel
+import com.example.projectexcursions.ui.auth.AuthViewModelFactory
 
-class ProfileFragment:Fragment(R.layout.fragment_profile) {
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    private lateinit var authViewModel: AuthViewModel
+    private lateinit var tokenRepository: TokenRepository // Объявляем переменную
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Получаем контекст приложения и передаем его в TokenRepository
+        tokenRepository = TokenRepository(requireContext())
+
+        // Инициализация ViewModel
+        val factory = AuthViewModelFactory(tokenRepository)
+        authViewModel = ViewModelProvider(this, factory).get(AuthViewModel::class.java)
+
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
-        if (isUserAuthenticated()) {
 
-
-        } else {
-
-
-            startAuthActivity()
-        }
+        // Проверка аутентификации
+        authViewModel.checkAndDecodeToken().observe(viewLifecycleOwner, Observer { claims ->
+            if (claims != null) {
+                // Пользователь аутентифицирован, можете выполнить действия для отображения профиля
+                
+            } else {
+                // Пользователь не аутентифицирован, переходим на экран аутентификации
+                startAuthActivity()
+            }
+        })
 
         return view
-    }
-
-    private fun isUserAuthenticated(): Boolean {
-        return false
     }
 
     private fun startAuthActivity() {
@@ -41,5 +49,3 @@ class ProfileFragment:Fragment(R.layout.fragment_profile) {
         startActivity(intent)
     }
 }
-
-
