96aacc78ea271d0249fea151dd1ffd65965c43f5
dvrk_rave <ivorkov105@gmail.com>
2024-12-14 15:23:23 +0700

    прикрутил карту (просто так)

--- app/src/main/java/com/example/projectexcursions/ui/fragments/MapFragment.kt
+++
@@ -1,6 +0,0 @@
-import androidx.fragment.app.Fragment
-import com.example.projectexcursions.R
-
-class MapFragment:Fragment(R.layout.fragment_map) {
-
-}
---
+++ app/src/main/java/com/example/projectexcursions/ui/map/MapFragment.kt
@@ -0,0 +1,47 @@
+package com.example.projectexcursions.ui.map
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.projectexcursions.R
+import com.example.projectexcursions.databinding.FragmentMapBinding
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.mapview.MapView
+
+class MapFragment:Fragment(R.layout.fragment_map) {
+
+    private lateinit var mapView: MapView
+    private lateinit var binding: FragmentMapBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        MapKitFactory.initialize(requireContext())
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMapBinding.inflate(inflater, container, false)
+        mapView = binding.mapview
+
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        MapKitFactory.getInstance().onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+}
