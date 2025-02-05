63aecd3e24b7ee17fab4c72c5565c9952decac10
dvrk_rave <ivorkov105@gmail.com>
2025-02-05 18:29:59 +0700

    пофиксил пагинацию на нашей стороне, но решение временное, ждём фикса от бэка

--- app/src/main/java/com/example/projectexcursions/ui/map/MapViewModel.kt
+++ app/src/main/java/com/example/projectexcursions/ui/map/MapViewModel.kt
@@ -1,13 +1,9 @@
 package com.example.projectexcursions.ui.map
 
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-import android.Manifest
-import android.content.pm.PackageManager
 
 @HiltViewModel
-class MapViewModel @Inject constructor(): ViewModel()  {
-
+class MapViewModel @Inject constructor() : ViewModel() {
 }
