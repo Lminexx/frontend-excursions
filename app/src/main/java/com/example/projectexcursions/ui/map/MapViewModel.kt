3cf57748939eb5ac3de38dd662bd8c21b06864b0
dvrk_rave <ivorkov105@gmail.com>
2025-02-24 23:23:13 +0700

    сижу делаю карту, пока что тестовые наброски просто для того, чтобы отобразить какую то точку, есть пара ошибок, например, не отрисовывается иконка точки (location_pin_ic.xml), импортировал также, как и всё остальное, также вылетает такая ошибка: WrPKh0hB5TeE3RaKMs1c: AndroidBitmap_getInfo() failed! Error code: -1 ну и ещё что то странное с api ключом, я помню, возможно, что то недописал, пока что такой скудный прогресс

--- app/src/main/java/com/example/projectexcursions/ui/map/MapViewModel.kt
+++ app/src/main/java/com/example/projectexcursions/ui/map/MapViewModel.kt
@@ -1,9 +1,19 @@
 package com.example.projectexcursions.ui.map
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.yandex.mapkit.geometry.Point
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MapViewModel @Inject constructor() : ViewModel() {
+
+    private val _curPoint = MutableLiveData<Point>()
+    val curPoint: LiveData<Point> get() = _curPoint
+
+    fun point(point: Point) {
+        _curPoint.value = point
+    }
 }
