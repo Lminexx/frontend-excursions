6f4c72a574ec717c6385fc9a424bdc3d775248b6
dvrk_rave <ivorkov105@gmail.com>
2025-02-25 04:29:32 +0700

    в какой то момент удалось всё пофиксить с константной точкой, но сейчас почему то не работает, не вызывается метод viewModel.startLocationTracker, хотя в логах видно, что разрешения предоставлены

--- app/src/main/java/com/example/projectexcursions/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/projectexcursions/ui/map/MapFragment.kt
@@ -9,23 +9,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.projectexcursions.BuildConfig
 import com.example.projectexcursions.R
 import com.example.projectexcursions.databinding.FragmentMapBinding
+import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.IconStyle
 import com.yandex.mapkit.map.MapObjectTapListener
 import com.yandex.mapkit.map.PlacemarkMapObject
+import com.yandex.mapkit.map.TextStyle
 import com.yandex.mapkit.mapview.MapView
 import com.yandex.runtime.image.ImageProvider
 
 class MapFragment : Fragment(R.layout.fragment_map) {
 
+    private val REQUEST_CODE_PERMISSION = 1004
+
     private val viewModel: MapViewModel by viewModels()
-    private val curPoint: Point = Point(55.751225, 37.62954) //просто для тестов
     private lateinit var placemark: PlacemarkMapObject
     private lateinit var mapView: MapView
     private lateinit var binding: FragmentMapBinding
@@ -33,11 +38,7 @@ class MapFragment : Fragment(R.layout.fragment_map) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("onCreate", "")
-        checkPermissions()
 
-        MapKitFactory.setApiKey(BuildConfig.MAPKIT_API_KEY)
-        Log.d("YandexMap", "API Key: ${BuildConfig.MAPKIT_API_KEY}")
-        MapKitFactory.initialize(requireContext())
     }
 
     override fun onCreateView(
@@ -48,21 +49,37 @@ class MapFragment : Fragment(R.layout.fragment_map) {
         Log.d("onCreateView", "")
         binding = FragmentMapBinding.inflate(inflater, container, false)
         mapView = binding.mapview
-        viewModel.point(curPoint)
         placemark = mapView.mapWindow.map.mapObjects.addPlacemark()
 
+        checkPermissions()
         subscribe()
         initCallback()
 
         return binding.root
     }
 
+    override fun onResume() {
+        super.onResume()
+        checkPermissions()
+    }
+
     private fun initCallback() {
+        Log.d("initCallback","")
+        val placemarkTapListener = MapObjectTapListener { _, point ->
+            Toast.makeText(
+                requireContext(),
+                "Tapped the point (${point.longitude}, ${point.latitude})",
+                Toast.LENGTH_SHORT
+            ).show()
+            true
+        }
         placemark.addTapListener(placemarkTapListener)
     }
 
     private fun subscribe() {
+        Log.d("subscribe","")
         viewModel.curPoint.observe(viewLifecycleOwner) {curPoint ->
+            Log.d("subscribe","curPoint: ${curPoint.latitude}, ${curPoint.longitude}")
             setOnCreateLocation(curPoint)
             setPin(curPoint)
         }
@@ -70,40 +87,54 @@ class MapFragment : Fragment(R.layout.fragment_map) {
 
     override fun onStart() {
         super.onStart()
+        Log.d("onStart","")
         MapKitFactory.getInstance().onStart()
         mapView.onStart()
     }
 
     override fun onStop() {
         mapView.onStop()
+        Log.d("onStart","")
         MapKitFactory.getInstance().onStop()
         super.onStop()
     }
 
-
-    private val placemarkTapListener = MapObjectTapListener { _, point ->
-        Toast.makeText(
-            requireContext(),
-            "Tapped the point (${point.longitude}, ${point.latitude})",
-            Toast.LENGTH_SHORT
-        ).show()
-        true
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.d("onRequestPermissionsResult", "")
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                || grantResults.getOrNull(1) == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Permissions", "Granted")
+                viewModel.startLocationTracker()
+            } else {
+                Toast.makeText(requireContext(), "Разрешение не предоставлено", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun checkPermissions() {
-        if (ActivityCompat.checkSelfPermission(
-            requireContext(),
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-            requireContext(),
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        ) != PackageManager.PERMISSION_GRANTED) {
-            val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
-            ActivityCompat.requestPermissions(requireActivity(), permissions, 0)
+        Log.d("checkPermissions","ACCESS_FINE_LOCATION = ${Manifest.permission.ACCESS_FINE_LOCATION}, \n" +
+                "ACCESS_COARSE_LOCATION = ${Manifest.permission.ACCESS_COARSE_LOCATION}")
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            Log.d("Permissions Granted", "YEES")
+            viewModel.startLocationTracker()
+        } else {
+            Log.d("Permissions Denied", "no(((")
+            ActivityCompat.requestPermissions(requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                REQUEST_CODE_PERMISSION)
         }
     }
 
     private fun setOnCreateLocation(point:Point) {
+        Log.d("setOnCreateLocation","")
         try {
             mapView.mapWindow.map.move(
                 CameraPosition(
@@ -111,7 +142,9 @@ class MapFragment : Fragment(R.layout.fragment_map) {
                     17.0f,
                     150.0f,
                     30.0f
-                )
+                ),
+                Animation(Animation.Type.SMOOTH, 1f),
+                null
             )
         } catch (eNull: NullPointerException) {
             Toast.makeText(requireContext(), "Индиана Джонс нашёл неприятный артефакт", Toast.LENGTH_SHORT).show()
@@ -119,10 +152,22 @@ class MapFragment : Fragment(R.layout.fragment_map) {
     }
 
     private fun setPin(point: Point) {
-        val imageProvider = ImageProvider.fromResource(requireContext(), R.drawable.location_pin_ic)
+        Log.d("setPin","")
+        val imageProvider = ImageProvider.fromResource(requireContext(), R.drawable.ic_location_pin)
         placemark.apply {
+            val iconStyle = IconStyle().apply { scale = 0.4f }
             geometry = point
-            setIcon(imageProvider)
+            setIcon(imageProvider, iconStyle)
+            setText(
+                "",
+                TextStyle()
+            )
         }
     }
+    /*
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("onViewCreated","")
+        viewModel.startLocationTracker()
+    }*/
 }
--- app/src/main/java/com/example/projectexcursions/ui/map/MapViewModel.kt
+++ app/src/main/java/com/example/projectexcursions/ui/map/MapViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.projectexcursions.ui.map
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.location.Location
+import com.yandex.mapkit.location.LocationListener
+import com.yandex.mapkit.location.LocationStatus
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -13,7 +18,23 @@ class MapViewModel @Inject constructor() : ViewModel() {
     private val _curPoint = MutableLiveData<Point>()
     val curPoint: LiveData<Point> get() = _curPoint
 
-    fun point(point: Point) {
-        _curPoint.value = point
+    fun startLocationTracker() {
+        val locationManager = MapKitFactory.getInstance().createLocationManager()
+        locationManager.requestSingleUpdate(object : LocationListener {
+            override fun onLocationUpdated(location: Location) {
+                Log.d("startLocationTracker", "onLocationUpdated")
+                _curPoint.value = location.position
+            }
+
+            override fun onLocationStatusUpdated(locationStatus: LocationStatus) {
+                Log.d("startLocationTracker", "onLocationStatusUpdated")
+                when (locationStatus) {
+                    LocationStatus.NOT_AVAILABLE -> Log.e("Location", "Not available")
+                    LocationStatus.AVAILABLE -> Log.d("Location", "Available")
+                    LocationStatus.RESET -> Log.d("Location", "Reset")
+                }
+            }
+
+        })
     }
 }
