b9aac66aa3972207726961faf08c56396d570aee
dvrk_rave <ivorkov105@gmail.com>
2025-03-03 20:02:48 +0700

    сделал построение маршрутов на главной карте, это было долго, это было больно, это было страшно, я задумался сотню раз о жизни, но всё же успех, я рад!

--- app/src/main/java/com/example/projectexcursions/repositories/pointrepo/PointRepositoryImpl.kt
+++ app/src/main/java/com/example/projectexcursions/repositories/pointrepo/PointRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.projectexcursions.repositories.pointrepo
 
+import android.util.Log
 import com.yandex.mapkit.geometry.Point
 import javax.inject.Inject
 
@@ -13,35 +14,45 @@ class PointRepositoryImpl @Inject constructor(): PointRepository {
 
     override fun cacheStart(point: Point) {
         cachedStart = point
+        Log.d("cachedStart", "${cachedStart?.longitude}, ${cachedStart?.latitude}")
     }
 
     override fun cacheEnd(point: Point) {
         cachedEnd = point
+        Log.d("cachedEnd", "${cachedEnd?.longitude}, ${cachedEnd?.latitude}")
     }
 
     override fun getCachedStart(): Point? {
+        Log.d("cachedStart", "${cachedStart?.longitude}, ${cachedStart?.latitude}")
         return cachedStart
     }
 
     override fun getCachedEnd(): Point? {
+        Log.d("cachedEnd", "${cachedEnd?.longitude}, ${cachedEnd?.latitude}")
         return cachedEnd
     }
 
     override fun deleteDCachedStart() {
         cachedStart = null
+        Log.d("cachedStart", "${cachedStart?.longitude}, ${cachedStart?.latitude}")
     }
 
     override fun deleteCachedEnd() {
         cachedEnd = null
+        Log.d("cachedEnd", "${cachedEnd?.longitude}, ${cachedEnd?.latitude}")
     }
 
     override fun deleteCachedPoints() {
         cachedEnd = null
         cachedStart = null
+        Log.d("cachedStart", "${cachedStart?.longitude}, ${cachedStart?.latitude}")
+        Log.d("cachedEnd", "${cachedEnd?.longitude}, ${cachedEnd?.latitude}")
     }
 
     override fun setRoute(points: List<Point>) {
         cachedRoute = points
+        Log.d("cachedRoute 1 Point", "${cachedRoute?.get(1)?.longitude.toString()}, ${cachedRoute?.get(1)?.latitude.toString()}")
+        Log.d("cachedRoute Last Point", "${cachedRoute?.get(cachedRoute!!.lastIndex)?.longitude.toString()}, ${cachedRoute?.get(cachedRoute!!.lastIndex)?.latitude.toString()}")
     }
 
     override fun getRoute(): List<Point>? {
@@ -53,6 +64,6 @@ class PointRepositoryImpl @Inject constructor(): PointRepository {
     }
 
     override fun hasRoute(): Boolean {
-        return cachedRoute?.equals(null) ?: false
+        return cachedRoute != null
     }
 }
