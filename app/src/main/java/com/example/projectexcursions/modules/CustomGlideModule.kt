adf4baaf37ccf1aac76ebd5f80138579583b2bca
Deni Erdyneev <erdeni.bat@gmail.com>
2025-05-21 16:39:24 +0700

    Disable glide cert path validation

--- app/src/main/java/com/example/projectexcursions/modules/CustomGlideModule.kt
+++ app/src/main/java/com/example/projectexcursions/modules/CustomGlideModule.kt
@@ -1,5 +1,6 @@
 package com.example.projectexcursions.modules
 
+import android.annotation.SuppressLint
 import android.content.Context
 import com.bumptech.glide.Glide
 import com.bumptech.glide.Registry
@@ -13,6 +14,10 @@ import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import java.io.InputStream
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.X509TrustManager
 
 @GlideModule
 class CustomGlideModule : AppGlideModule() {
@@ -32,7 +37,34 @@ class CustomGlideModule : AppGlideModule() {
         registry.replace(
             GlideUrl::class.java,
             InputStream::class.java,
-            OkHttpUrlLoader.Factory(entryPoint.okHttpClientBuilder().build())
+            OkHttpUrlLoader.Factory(
+                entryPoint.okHttpClientBuilder()
+                    .disableCertPathValidation()
+                    .build()
+            )
         )
+    }
+    
+    @SuppressLint("TrulyRandom")
+    fun OkHttpClient.Builder.disableCertPathValidation(): OkHttpClient.Builder {
+        val sslContext = SSLContext.getInstance("SSL")
+        val trustManager = DisabledTrustManager()
+        sslContext.init(null, arrayOf(trustManager), SecureRandom())
+        sslSocketFactory(sslContext.socketFactory, trustManager)
+        return hostnameVerifier { _, _ -> true }
+    }
+
+    @SuppressLint("TrustAllX509TrustManager,CustomX509TrustManager")
+    private class DisabledTrustManager : X509TrustManager {
+
+        override fun getAcceptedIssuers(): Array<out X509Certificate> = arrayOf()
+
+        override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {
+            // empty
+        }
+
+        override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {
+            // empty
+        }
     }
 }
