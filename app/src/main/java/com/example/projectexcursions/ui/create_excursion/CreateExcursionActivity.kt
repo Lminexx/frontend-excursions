1b302af7083ffa0e170670851fe6f1bf7f30aa78
Feduk <zfed2004@mail.ru>
2025-05-24 14:15:25 +0700

    Доделал добавление/удаление фоток экскурсии при ее редактировании или создании

--- app/src/main/java/com/example/projectexcursions/ui/create_excursion/CreateExcursionActivity.kt
+++ app/src/main/java/com/example/projectexcursions/ui/create_excursion/CreateExcursionActivity.kt
@@ -287,7 +287,8 @@ class CreateExcursionActivity : AppCompatActivity() {
                 val city = binding.cityName.text.toString()
                 val topic = translateTopic(binding.topic.selectedItem.toString())
                 val selected = viewModel.selectedImages.value ?: emptyList()
-                val existing = viewModel.images.value?.map { photo -> photo.url.toUri() } ?: emptyList()
+                val existing =
+                    viewModel.images.value?.map { photo -> photo.url.toUri() } ?: emptyList()
                 val images = selected + existing
                 if (viewModel.isExcursionCorrect(this, title, description, places, city, images)) {
                     viewModel.editExcursion(
@@ -324,7 +325,8 @@ class CreateExcursionActivity : AppCompatActivity() {
                 val city = binding.cityName.text.toString()
                 val topic = translateTopic(binding.topic.selectedItem.toString())
                 val selected = viewModel.selectedImages.value ?: emptyList()
-                val existing = viewModel.images.value?.map { photo -> photo.url.toUri() } ?: emptyList()
+                val existing =
+                    viewModel.images.value?.map { photo -> photo.url.toUri() } ?: emptyList()
                 val images = selected + existing
                 if (viewModel.isExcursionCorrect(this, title, description, places, city, images)) {
                     viewModel.createExcursion(
